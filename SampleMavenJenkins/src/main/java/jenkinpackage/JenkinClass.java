4ee91ac8ccf844b9b2cad64388612bc4a157be57
SHARONJAYARAM <sharonjayaram96@gmail.com>
2023-06-26 11:51:40 +0530

    Jenkin sample

---
+++ SampleMavenJenkins/src/main/java/jenkinpackage/JenkinClass.java
@@ -0,0 +1,34 @@
+package jenkinpackage;
+
+public class JenkinClass {
+	
+	
+	
+	
+	
+	
+
+		
+		public static  void print()
+		{
+			System.out.println("Hello World");
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			JenkinClass.print();
+
+		}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
